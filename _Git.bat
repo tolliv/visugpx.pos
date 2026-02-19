caf102a531e8a12930d57443b338111b5a84e59c
Thierry OLLIVIER <thierry.ollivier@spaceable.org>
2026-02-19 21:51:42 +0100

    Auto-update 19/02/2026 21:51:42.74

---
+++ _Git.bat
@@ -0,0 +1,8 @@
+@echo off
+set msg=Auto-update %date% %time%
+c:\git\bin\git add .
+c:\git\bin\git commit -m "%msg%"
+c:\git\bin\git push origin main
+
+echo Mise a jour envoyee avec le message : %msg%
+timeout /t 3
